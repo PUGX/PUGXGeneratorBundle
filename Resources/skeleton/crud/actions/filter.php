88ac453b3d7ca3acf116bf30c455b31f96d367dc
Massimiliano Arione <garakkio@gmail.com>
2013-04-08 14:25:37 +0200

    improve filters handling

--- Resources/skeleton/crud/actions/filter.php
+++ Resources/skeleton/crud/actions/filter.php
@@ -12,14 +12,12 @@
     {
         $request = $this->getRequest();
         $url = is_null($route) ? $this->generateUrl($name) : $this->generateUrl($route, $params);
-        if ($request->query->has('submit-filter')) {
-            if ($form->bind($request)->isValid()) {
-                $this->setFilter($form->getData(), $name);
+        if ($request->query->has('submit-filter') && $form->bind($request)->isValid()) {
+            $request->getSession()->set('filter.' . $name, $request->get($form->getName()));
 
-                return $this->redirect($url);
-            }
+            return $this->redirect($url);
         } elseif ($request->query->has('reset-filter')) {
-            $this->resetFilter($name);
+            $request->getSession()->set('filter.' . $name, null);
 
             return $this->redirect($url);
         }
@@ -35,38 +33,12 @@
     protected function filter(FormInterface $form, QueryBuilder $qb, $name)
     {
         if (!is_null($values = $this->getFilter($name))) {
-            $v = array_map(array($this, 'convertEntity'), $values);
-            if ($form->bind($v)->isValid()) {
+            if ($form->bind($values)->isValid()) {
                 $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $qb);
             }
         }
 
         return $this->get('knp_paginator')->paginate($qb->getQuery(), $this->getRequest()->query->get('page', 1), 20);
-    }
-
-    /**
-     * Save filters in session, in a fieldName/fieldValue array
-     *
-     * @param mixed  $entity
-     * @param string $name
-     */
-    protected function setFilter($entity, $name)
-    {
-        $values = array();
-        $array = (array) $entity;
-        foreach ($array as $property => $value) {
-            if (is_null($value) || ($value instanceof ArrayCollection && count($value) == 0)) {
-                continue;
-            }
-            if (is_object($value) && is_callable(array($value, 'getId')))
-            {
-                $value = $value->getId();
-            }
-            // workaround: keys are like '�*�id'
-            $values[substr($property, 3)] = $value;
-        }
-
-        $this->getRequest()->getSession()->set('filter.' . $name, $values);
     }
 
     /**
@@ -78,33 +50,4 @@
     protected function getFilter($name)
     {
         return $this->getRequest()->getSession()->get('filter.' . $name);
-    }
-
-    /**
-     * Reset filters (delete them from session)
-     *
-     * @param  string $name
-     */
-    protected function resetFilter($name)
-    {
-        $this->getRequest()->getSession()->set('filter.' . $name, null);
-    }
-
-    /**
-     * Converts entities to their ids
-     *
-     * @param  mixed $value
-     * @return mixed
-     * @see    filter()
-     */
-    private function convertEntity($value)
-    {
-        if (is_object($value) && get_class($value) == 'Doctrine\Common\Collections\ArrayCollection') {
-            return $value->toArray();
-        }
-        if (is_object($value) && is_callable(array($value, 'getId'))) {
-            return $value->getId();
-        }
-
-        return $value;
     }
