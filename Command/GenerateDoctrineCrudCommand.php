dc0548640271f7b96ce767e064188d95baa8e044
Leonardo Proietti <leonardo.proietti@gmail.com>
2013-08-08 12:18:54 +0200

    Update GenerateDoctrineCrudCommand.php
    fix signature for getSkeletonDirs()

--- Command/GenerateDoctrineCrudCommand.php
+++ Command/GenerateDoctrineCrudCommand.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\GeneratorBundle\Command\Validators;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 /**
  * Generates a CRUD for a Doctrine entity.
@@ -187,7 +188,7 @@ EOT
      *
      * @return array
      */
-    protected function getSkeletonDirs($bundle = null)
+    protected function getSkeletonDirs(BundleInterface $bundle = null)
     {
         $baseSkeletonDirs = parent::getSkeletonDirs($bundle);
 
